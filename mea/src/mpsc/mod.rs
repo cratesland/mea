15993b5370a422e4863ae12c104fc4857a2d0fc1
tison <wander4096@gmail.com>
2025-05-31 13:14:06 +0800

    rework use std mpsc
    Signed-off-by: tison <wander4096@gmail.com>

--- mea/src/mpsc/mod.rs
+++ mea/src/mpsc/mod.rs
@@ -1,71 +1,285 @@
-//!
+// Copyright 2024 tison <wander4096@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-use crate::internal::Mutex;
-use std::collections::VecDeque;
+//! A multi-producer, single-consumer queue for sending values between asynchronous tasks.
+
+use std::fmt;
 use std::future::poll_fn;
+use std::ptr::null_mut;
+use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
-use std::task::{Context, Poll, Waker};
+use std::task::Context;
+use std::task::Poll;
+use std::task::Waker;
 
 #[cfg(test)]
 mod tests;
 
+/// Creates an unbounded mpsc channel for communicating between asynchronous
+/// tasks without backpressure.
 ///
+/// A `send` on this channel will always succeed as long as the receiver is alive.
+/// If the receiver falls behind, messages will be arbitrarily buffered.
+///
+/// Note that the amount of available system memory is an implicit bound to
+/// the channel. Using an `unbounded` channel has the ability of causing the
+/// process to run out of memory. In this case, the process will be aborted.
 pub fn unbounded<T>() -> (UnboundedSender<T>, UnboundedReceiver<T>) {
-    let state = Arc::new(Mutex::new(UnboundedState {
-        buffer: VecDeque::new(),
-        rx_task: None,
-    }));
-
-    (
-        UnboundedSender {
-            state: state.clone(),
-        },
-        UnboundedReceiver {
-            state: state.clone(),
-        },
-    )
+    let state = Arc::new(UnboundedState {
+        rx_task: AtomicPtr::new(null_mut()),
+    });
+    let (sender, receiver) = std::sync::mpsc::channel();
+    let sender = UnboundedSender {
+        state: state.clone(),
+        sender,
+    };
+    let receiver = UnboundedReceiver {
+        state: state.clone(),
+        receiver,
+    };
+    (sender, receiver)
 }
 
+struct UnboundedState {
+    rx_task: AtomicPtr<Waker>,
+}
+
+/// Send values to the associated [`UnboundedReceiver`].
 ///
+/// Instances are created by the [`unbounded`] function.
 #[derive(Clone)]
 pub struct UnboundedSender<T> {
-    state: Arc<Mutex<UnboundedState<T>>>,
+    state: Arc<UnboundedState>,
+    sender: std::sync::mpsc::Sender<T>,
 }
 
-impl<T> UnboundedSender<T> {
-    ///
-    pub fn send(&self, value: T) {
-        let mut state = self.state.lock();
-        state.buffer.push_back(value);
-        if let Some(waker) = state.rx_task.take() {
-            waker.wake();
+impl<T> fmt::Debug for UnboundedSender<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("UnboundedSender").finish_non_exhaustive()
+    }
+}
+
+impl<T> Drop for UnboundedSender<T> {
+    fn drop(&mut self) {
+        // If the receiver has been dropped, wake up the task that is waiting for a message.
+        let waker = self.state.rx_task.swap(null_mut(), Ordering::AcqRel);
+        if !waker.is_null() {
+            unsafe { (*waker).wake_by_ref() }
         }
     }
 }
 
+impl<T> UnboundedSender<T> {
+    /// Attempts to send a message without blocking.
+    ///
+    /// This method is not marked async because sending a message to an unbounded channel
+    /// never requires any form of waiting. Because of this, the `send` method can be
+    /// used in both synchronous and asynchronous code without problems.
+    ///
+    /// If the receiver has been dropped, this function returns an error. The error includes
+    /// the value passed to `send`.
+    pub fn send(&self, value: T) -> Result<(), SendError<T>> {
+        self.sender.send(value).map_err(|err| SendError(err.0))?;
+        let waker = self.state.rx_task.swap(null_mut(), Ordering::AcqRel);
+        if !waker.is_null() {
+            unsafe { (*waker).wake_by_ref() }
+        }
+        Ok(())
+    }
+}
+
+/// An error returned when trying to send on a closed channel. Returned from
+/// [`UnboundedSender::send`] if the corresponding [`UnboundedReceiver`] has
+/// already been dropped.
 ///
+/// The message that could not be sent can be retrieved again with
+/// [`SendError::into_inner`].
+pub struct SendError<T>(T);
+
+impl<T> SendError<T> {
+    /// Get a reference to the message that failed to be sent.
+    pub fn as_inner(&self) -> &T {
+        &self.0
+    }
+
+    /// Consumes the error and returns the message that failed to be sent.
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T> fmt::Display for SendError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        "sending on a closed channel".fmt(f)
+    }
+}
+
+impl<T> fmt::Debug for SendError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "SendError<{}>(..)", stringify!(T))
+    }
+}
+
+impl<T> std::error::Error for SendError<T> {}
+
+/// Receive values from the associated [`UnboundedSender`].
+///
+/// Instances are created by the [`unbounded_channel`] function.
 pub struct UnboundedReceiver<T> {
-    state: Arc<Mutex<UnboundedState<T>>>,
+    state: Arc<UnboundedState>,
+    receiver: std::sync::mpsc::Receiver<T>,
+}
+
+impl<T> fmt::Debug for UnboundedReceiver<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("UnboundedReceiver")
+            .finish_non_exhaustive()
+    }
 }
 
 impl<T> UnboundedReceiver<T> {
+    /// Tries to receive the next value for this receiver.
     ///
+    /// This method returns the [`Empty`] error if the channel is currently
+    /// empty, but there are still outstanding [senders].
+    ///
+    /// This method returns the [`Disconnected`] error if the channel is
+    /// currently empty, and there are no outstanding [senders].
+    ///
+    /// [`Empty`]: TryRecvError::Empty
+    /// [`Disconnected`]: TryRecvError::Disconnected
+    /// [`poll_recv`]: Self::poll_recv
+    /// [senders]: UnboundedSender
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// use mea::mpsc;
+    /// use mea::mpsc::TryRecvError;
+    /// let (tx, mut rx) = mpsc::unbounded();
+    ///
+    /// tx.send("hello").unwrap();
+    ///
+    /// assert_eq!(Ok("hello"), rx.try_recv());
+    /// assert_eq!(Err(TryRecvError::Empty), rx.try_recv());
+    ///
+    /// tx.send("hello").unwrap();
+    /// drop(tx);
+    ///
+    /// assert_eq!(Ok("hello"), rx.try_recv());
+    /// assert_eq!(Err(TryRecvError::Disconnected), rx.try_recv());
+    /// # }
+    /// ```
+    pub fn try_recv(&mut self) -> Result<T, TryRecvError> {
+        match self.receiver.try_recv() {
+            Ok(v) => Ok(v),
+            Err(std::sync::mpsc::TryRecvError::Disconnected) => Err(TryRecvError::Disconnected),
+            Err(std::sync::mpsc::TryRecvError::Empty) => Err(TryRecvError::Empty),
+        }
+    }
+
+    /// Receives the next value for this receiver.
+    ///
+    /// This method returns `None` if the channel has been closed and there are
+    /// no remaining messages in the channel's buffer. This indicates that no
+    /// further values can ever be received from this `Receiver`. The channel is
+    /// closed when all senders have been dropped.
+    ///
+    /// If there are no messages in the channel's buffer, but the channel has
+    /// not yet been closed, this method will sleep until a message is sent or
+    /// the channel is closed.
+    ///
+    /// # Cancel safety
+    ///
+    /// This method is cancel safe. If `recv` is used as the event in a `select` statement
+    /// and some other branch completes first, it is guaranteed that no messages were received
+    /// on this channel.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// use mea::mpsc;
+    /// let (tx, mut rx) = mpsc::unbounded();
+    ///
+    /// tokio::spawn(async move {
+    ///     tx.send("hello").unwrap();
+    /// });
+    ///
+    /// assert_eq!(Some("hello"), rx.recv().await);
+    /// assert_eq!(None, rx.recv().await);
+    /// # }
+    /// ```
+    ///
+    /// Values are buffered:
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// use mea::mpsc;
+    /// let (tx, mut rx) = mpsc::unbounded();
+    ///
+    /// tx.send("hello").unwrap();
+    /// tx.send("world").unwrap();
+    ///
+    /// assert_eq!(Some("hello"), rx.recv().await);
+    /// assert_eq!(Some("world"), rx.recv().await);
+    /// # }
+    /// ```
     pub async fn recv(&mut self) -> Option<T> {
         poll_fn(|cx| self.poll_recv(cx)).await
     }
 
     fn poll_recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
-        let mut state = self.state.lock();
-        if let Some(value) = state.buffer.pop_front() {
-            Poll::Ready(Some(value))
-        } else {
-            state.rx_task = Some(cx.waker().clone());
-            Poll::Pending
+        match self.receiver.try_recv() {
+            Ok(v) => Poll::Ready(Some(v)),
+            Err(std::sync::mpsc::TryRecvError::Disconnected) => Poll::Ready(None),
+            Err(std::sync::mpsc::TryRecvError::Empty) => {
+                let waker = Box::into_raw(Box::new(cx.waker().clone()));
+                self.state.rx_task.store(waker, Ordering::Release);
+
+                match self.receiver.try_recv() {
+                    Ok(v) => Poll::Ready(Some(v)),
+                    Err(std::sync::mpsc::TryRecvError::Disconnected) => Poll::Ready(None),
+                    Err(std::sync::mpsc::TryRecvError::Empty) => Poll::Pending,
+                }
+            }
         }
     }
 }
 
-struct UnboundedState<T> {
-    buffer: VecDeque<T>,
-    rx_task: Option<Waker>,
+/// Error returned by `try_recv`.
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum TryRecvError {
+    /// This channel is currently empty, but the sender(s) have not yet disconnected, so data may
+    /// yet become available.
+    Empty,
+    /// The sender has become disconnected, and there will never be any more data received on it.
+    Disconnected,
 }
+
+impl fmt::Display for TryRecvError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            TryRecvError::Empty => "receiving on an empty channel".fmt(fmt),
+            TryRecvError::Disconnected => "receiving on a closed channel".fmt(fmt),
+        }
+    }
+}
+
+impl std::error::Error for TryRecvError {}
