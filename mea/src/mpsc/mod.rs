b18b5f2cf5c2015d48f19b4eb6d589aaff929f97
tison <wander4096@gmail.com>
2025-05-31 19:21:48 +0800

    redo
    Signed-off-by: tison <wander4096@gmail.com>

--- mea/src/mpsc/mod.rs
+++ mea/src/mpsc/mod.rs
@@ -167,7 +167,8 @@ pub struct UnboundedReceiver<T> {
     receiver: std::sync::mpsc::Receiver<T>,
 }
 
-unsafe impl<T: Send> Send for UnboundedReceiver<T> {}
+/// The only `!Sync` field `receiver` is protected by `&mut self` in `recv` and `try_recv`.
+/// That is, `UnboundedReceiver` can only be accessed by one thread at a time.
 unsafe impl<T: Send> Sync for UnboundedReceiver<T> {}
 
 impl<T> fmt::Debug for UnboundedReceiver<T> {
